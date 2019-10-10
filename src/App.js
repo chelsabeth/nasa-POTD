707810ab3eaf537f75ca3a3c1a51800a8ff26886
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-10-10 11:51:36 -0400

    date picker displaying, just no functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import NasaGrid from "./Components/NasaGrid";
+import '../node_modules/pikaday/css/pikaday.css';
 
 function App() {
   return (
     <div className="App">
+      <h3>Pick A Date:</h3><input type="date" id="datepicker"></input>
      <h1>Welcome Space Lovers! <span>🚀 👽 👾</span></h1>
      <NasaGrid /> 
     </div>
--- src/Components/NasaGrid.js
+++ src/Components/NasaGrid.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import NasaCard from "./NasaCard";
 
+
+
 export default function NasaGrid() {
   const [data, setData] = useState([]);
 
