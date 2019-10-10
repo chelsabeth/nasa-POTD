80256bce06b5325ade26ab5c2f77c00f7bd7f628
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-10-10 18:26:34 -0400

    date picker baby :)

--- src/Components/NasaCard.js
+++ src/Components/NasaCard.js
@@ -2,13 +2,14 @@
 import React from "react";
 
 const NasaCard = props => {
+  console.log("This is the card", props.data);
   return (
-    <div key={props.key}>
-      <h2>Title: {props.title}</h2>
-      <img alt="space photo" src={props.imgUrl} />
-      <h3>Photographer: {props.photographer}</h3>
-      <p>Date Taken: {props.date}</p>
-      <p>Description: {props.description}</p>
+    <div key={props.data.key}>
+      <h2>Title: {props.data.title}</h2>
+      <img alt="space photo" src={props.data.url} />
+      <h3>Photographer: {props.data.copyright}</h3>
+      <p>Date Taken: {props.data.date}</p>
+      <p>Description: {props.data.explanation}</p>
     </div>
   );
 };
--- src/Components/NasaGrid.js
+++ src/Components/NasaGrid.js
@@ -1,36 +1,14 @@
 // This file is going to be my API request
 // Parent === App.js
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import NasaCard from "./NasaCard";
 
+export default function NasaGrid(props) {
 
-
-export default function NasaGrid() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14`
-      )
-      .then(response => {
-        console.log("Response from nasa data", response.data);
-        setData(response.data);
-      })
-      .catch(error => {
-        console.log("Sorry, an error has occured", error);
-      });
-  }, []);
 
   return (
     <NasaCard
-      key={data.index}
-      title={data.title}
-      imgUrl={data.hdurl}
-      date={data.date}
-      description={data.explanation}
-      photographer={data.copyright}
+      data={props.data}
     />
   );
 }
